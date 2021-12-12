608fa2f478898e3c863d37cead8fb66cbe5d302f
tamagram <tamadevelopment211@gmail.com>
2021-12-12 12:50:00 +0900

    :tada: First commit in frontend.

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButon";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
+      </header>
+    </div>
+  );
+}
+
+export default App;
