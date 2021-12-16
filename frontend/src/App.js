47edbd972e6260e4572e4d650741c38020d46c7c
AYATSUKO <cdrk1210470@gn.iwasaki.ac.jp>
2021-12-16 22:28:27 +0900

    :sparkles: Add User page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,36 @@ import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButon";
 import Profile from "./components/Profile";
 import Message from "./components/Message";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import User from "./pages/User"
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
   return (
-    // TODO: react-router-domを追加する
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <LoginButton />
-        <LogoutButton />
-        <Profile />
-        <Message />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={
+            isAuthenticated ? (
+              <User />
+            ) :
+              (
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <LoginButton />
+                  <Profile />
+                  <Message />
+                </header>
+              )
+          } />
+        </Routes>
+      </div>
+    </Router >
   );
 }
 
