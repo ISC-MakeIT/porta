dfb1f3f171debe2e15d7a1477fb8c1672de5888b
tamagram <tamadevelopment211@gmail.com>
2021-12-17 13:58:27 +0900

    :recycle: Refactor code.

--- backend/main.go
+++ backend/main.go
@@ -1,55 +1,7 @@
-// main.go
-
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-
-	"github.com/ISC-MakeIT/porta/backend/middleware"
-)
+import "github.com/ISC-MakeIT/porta/backend/router"
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading the .env file: %v", err)
-	}
-
-	router := gin.Default()
-
-	router.Use(cors.New(
-		cors.Config{
-			AllowOrigins:     []string{"http://localhost:3000"},
-			AllowCredentials: true,
-			AllowHeaders:     []string{"Authorization"},
-		},
-	))
-
-	// This route is always accessible.
-	router.Any("/api/public", func(ctx *gin.Context) {
-		response := map[string]string{
-			"message": "Hello from a public endpoint! You don't need to be authenticated to see this.",
-		}
-		ctx.JSON(http.StatusOK, response)
-	})
-
-	// This route is only accessible if the user has a valid access_token.
-	router.GET(
-		"/api/private",
-		middleware.EnsureValidToken(),
-		func(ctx *gin.Context) {
-			response := map[string]string{
-				"message": "Hello from a private endpoint! You need to be authenticated to see this.",
-			}
-			ctx.JSON(http.StatusOK, response)
-		},
-	)
-
-	log.Print("Server listening on http://localhost:3010")
-	if err := http.ListenAndServe("0.0.0.0:3010", router); err != nil {
-		log.Fatalf("There was an error with the http server: %v", err)
-	}
+	router.SetUpRouter()
 }
