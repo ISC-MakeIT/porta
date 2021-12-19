85e85d2ea67d84fdca8fa14f3f660086ae9ccbcb
AYATSUKO <cdrk1210470@gn.iwasaki.ac.jp>
2021-12-19 22:45:18 +0900

    ログイン画面～ログイン後(修正版)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
-import LoginButton from "./components/LoginButton";
-import styles from "./conveyor.module.css";
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
 import User from "./pages/User";
+import Home from "./pages/Home";
 import { useAuth0 } from '@auth0/auth0-react';
-import IconPORTA from "./IconPORTA.png";
 
 function App() {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -21,14 +19,7 @@ function App() {
               <User />
             ) :
               (
-                <>
-                <div id={styles.img}>
-                    <img src = {IconPORTA} width={710} height={171} alt="PORTA" />
-                </div>
-                <div id={styles.progress}>
-                    <LoginButton />
-                </div>
-                </>
+                <Home />
               )
           } />
         </Routes>
--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,5 +1,7 @@
 import  LogoutButton  from '../components/LogoutButton.jsx';
 
+// TODO: Headerコンポーネントをこの画面に追加する
+
 const User = () => {
   return (
     <div>
