039752d7e83db5259a208ec39bfd6989aa8d1d0e
tamagram <tamadevelopment211@gmail.com>
2021-12-22 06:46:04 +0900

    :card_file_box: Redifine the schema.

--- backend/model/schema.go
+++ backend/model/schema.go
@@ -5,16 +5,17 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Auth0ID  string `gorm:"auth0_id" json:"auth0_id"`
-	Name     string
-	Nickname string
-	Picture  string
-	Email    string
-	Body     string
+	Name     string `gorm:"name" json:"name"`
+	Nickname string `gorm:"nickname" json:"nickname"`
+	Picture  string `gorm:"picture" json:"picture"`
+	Email    string `gorm:"email" json:"email"`
+	Body     string `gorm:"body" json:"body"`
 }
 
 type Post struct {
 	gorm.Model
-	UserID uint `gorm:"column:user_id" json:"user_id"`
-	Title  string
-	Body   string
+	UserID  uint   `gorm:"user_id" json:"user_id"`
+	Picture string `gorm:"picture" json:"picture"`
+	Title   string `gorm:"title" json:"title"`
+	Body    string `gorm:"body" json:"body"`
 }
