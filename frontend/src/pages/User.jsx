a971a3f39975b56e99ca462898e5f29e148365d7
tamagram <tamadevelopment211@gmail.com>
2021-12-21 15:26:25 +0900

    :sparkles: Implement User page.

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,16 +1,67 @@
-import LogoutButton from "../components/LogoutButton.jsx";
 import { useParams, Link } from "react-router-dom";
+import styles from "./User.module.css";
 
 // TODO: Headerコンポーネントをこの画面に追加する
 
 const User = () => {
   let params = useParams();
   return (
-    <div>
-      <h1>User {params.user_id}</h1>
-      <Link to={`/edit/${params.user_id}`}>Edit</Link>
-      <LogoutButton />
-    </div>
+    <>
+      <main class={styles.main}>
+        <div class={styles.left_container}>
+          <div class={styles.control}>
+            <img
+              class={styles.menu}
+              src="/icons/menu-black.svg"
+              alt="menu"
+            ></img>
+            <Link to={`/edit/${params.user_id}`}>
+              <img class={styles.edit} src="/icons/edit.svg" alt="edit"></img>
+            </Link>
+          </div>
+          <div class={styles.profile}>
+            <img
+              class={styles.avatar}
+              src="https://placehold.jp/216x216.png"
+              alt="user"
+            ></img>
+            <h1>UserName</h1>
+            <p>textt</p>
+            <div class={styles.icons}>
+              <img
+                class={styles.icon}
+                src="/icons/twitter.svg"
+                alt="twitter"
+              ></img>
+              <img
+                class={styles.icon}
+                src="/icons/instagram.svg"
+                alt="instagram"
+              ></img>
+            </div>
+          </div>
+        </div>
+        <div class={styles.right_container}>
+          <ul class={styles.posts}>
+            <li class={styles.post}>
+              <img src="https://placehold.jp/710x415.png" alt="something"></img>
+              <h1>title</h1>
+              <p>text</p>
+            </li>
+            <li class={styles.post}>
+              <img src="https://placehold.jp/710x415.png" alt="something"></img>
+              <h1>title</h1>
+              <p>text</p>
+            </li>
+            <li class={styles.post}>
+              <img src="https://placehold.jp/710x415.png" alt="something"></img>
+              <h1>title</h1>
+              <p>text</p>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </>
   );
 };
 
